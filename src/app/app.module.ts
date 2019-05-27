0eb2feb950daf9800f56e6c0dc8e18c52a02858f
David Liu <dvdgege5@gmail.com>
2019-05-27 17:01:28 -0400

    add router & route reuse strategy; add 404 not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// my imports
 import { AppComponent } from './app.component';
+import { MaltsevRouteReuseStrategy } from './utils/reuse-strategy-2';
+import { RouterModule, RouteReuseStrategy } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+
+
+// app router
+const appRoutes = [
+
+	// 404 not found
+	{ path: '404', component: NotFoundComponent },
+	{ path: '**', redirectTo: '404' },
+]
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NotFoundComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [
+		{ provide: RouteReuseStrategy, useClass: MaltsevRouteReuseStrategy },
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
