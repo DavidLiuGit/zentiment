c2731957ec40f5924aa6838767babff0d0a2faf2
David Ge Liu <ge.d.liu@intel.com>
2019-06-10 17:28:40 -0400

    add greeting to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+	dark:	boolean = true;		// enable dark mode by default
+	name:	string	= "user";
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
