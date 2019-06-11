0fa3670014500b7e87c051e99a9a8bf6c1f38fa1
David Ge Liu <ge.d.liu@intel.com>
2019-06-11 17:49:39 -0400

    remove notebook design in favor of a simpler and cleaner textarea

--- src/app/compose/freestyle/freestyle.component.ts
+++ src/app/compose/freestyle/freestyle.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
 	selector: 'app-freestyle',
 	templateUrl: './freestyle.component.html',
 	styleUrls: ['./freestyle.component.scss'],
 })
-export class FreestyleComponent implements OnInit {
-	
+export class FreestyleComponent implements OnInit, AfterViewInit {
+	@ViewChild("input") textbox	: ElementRef;
 
 	ngOnInit() {
+	}
+
+	ngAfterViewInit() {
+		this.textbox.nativeElement.focus();
 	}
 } 
