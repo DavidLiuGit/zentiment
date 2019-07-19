9c4f280c042aadd03982db9aa265bd5f1ae9b912
DL <dvdgege5@gmail.com>
2019-07-19 14:56:39 -0400

    modify server to store metadata along with the log itself

--- server/api_logs.py
+++ server/api_logs.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, g
 from pprint import pprint
+from datetime import datetime
 
 from database import MongoInterface
 
@@ -11,18 +12,53 @@ app_logs = Blueprint ( 'app_logs', __name__ )
 # consts
 
 
+###############################################################################
+##### API
+###############################################################################
 
 # save new log
-@app_logs.route('new', methods=['POST'])
-def new_log ():
-	# save new log to collection
-	posts = MongoInterface.connect().test.logs
-	post_id = posts.insert_one(request.json).inserted_id
+@app_logs.route('<user>/new', methods=['POST'])
+def new_log (user):
+	# parse the entry and get a Mongo-friendly dict to save
+	log = create_log(user, request.json)
+
+	# establish connection w/ database
+	posts = MongoInterface.connect().test[f"{user}_logs"]
+	post_id = posts.insert_one(log).inserted_id
 
 	# response
 	resp = { "post_id": str(post_id) }
 	return jsonify(resp), 200
 
+
+
+
+###############################################################################
+##### HELPERS
+###############################################################################
+
+def create_log (user, payload):
+	"""
+	Create a dictionary containing a log and metadata for the log. Metadata 
+	includes the current datetime, author, list of #tags in the log, etc.
+	"""
+	body = payload['entry']
+	dt = datetime.now()
+	entry = {
+		"user": user,
+		"_metadata": {
+			"date": str(dt.date()),
+			"time": str(dt.time()),
+			"weekday": str(dt.isoweekday()),
+			"count": {
+				"char": len(body),
+				"word": len(body.split()),
+			}
+		},
+		"log": body,
+	}
+
+	return entry
 
 
 
